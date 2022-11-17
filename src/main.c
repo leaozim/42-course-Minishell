596dc78de356110490960c7527799ba599beea9f
Emmanuel Tomiyoshi <tomiyoshiemmanuel@gmail.com>
2022-11-17 12:20:43 -0300

    added olar

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: etomiyos <etomiyos@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/17 11:41:54 by etomiyos          #+#    #+#             */
-/*   Updated: 2022/11/17 12:08:34 by etomiyos         ###   ########.fr       */
+/*   Updated: 2022/11/17 12:20:23 by etomiyos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,6 @@
 
 int main(int argc, char **argv, char **envp)
 {
-    printf("oi\n");
+    printf("olar\n");
     return (0);
 }
