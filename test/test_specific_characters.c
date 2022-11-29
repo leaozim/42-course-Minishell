fea0705f237eb343aec9bf16a832709a41892f95
Larissa <larissa_silva@outlook.com>
2022-11-29 14:29:23 -0300

    finished tokenization

--- test/test_specific_characters.c
+++ test/test_specific_characters.c
@@ -2,18 +2,44 @@
 
 void	check_token_count_squote()
 {
-	TEST_ASSERT_EQUAL_INT(11, 11);
-	TEST_ASSERT_EQUAL_INT(11, 11);
+	char	*str;
+
+	str = "cat \'oi\' \' \'";
+	TEST_ASSERT_EQUAL_INT(2, count_tokens_specific(str));
+
+	str = "echo \"oi \' dsdf\' \"";
+	TEST_ASSERT_EQUAL_INT(1, count_tokens_specific(str));
 }
 
 void	check_token_count_dquotes()
 {
-	TEST_ASSERT_EQUAL_INT(11, 11);
+	char	*str;
+	
+	str = "cat \"oi\" \" \"";
+	TEST_ASSERT_EQUAL_INT(2, count_tokens_specific(str));
+
+	str = "echo \"oi \' dsdf\' \"";
+	TEST_ASSERT_EQUAL_INT(1, count_tokens_specific(str));
 }
 
 void	check_token_count_mixed_quotes()
 {
-	TEST_ASSERT_EQUAL_INT(11, 11);
+	char	*str;
+
+	str = "cat \"oi\" \' \'";
+	TEST_ASSERT_EQUAL_INT(2, count_tokens_specific(str));
+
+	str = "cat \"oi         \" \'         \'";
+	TEST_ASSERT_EQUAL_INT(2, count_tokens_specific(str));
+
+	str = "cat \"oi \' \"";
+	TEST_ASSERT_EQUAL_INT(1, count_tokens_specific(str));
+
+	str = "cat \'oi\" \' ";
+	TEST_ASSERT_EQUAL_INT(1, count_tokens_specific(str));
+
+		str = "cat \"oi\"\'\'";
+	TEST_ASSERT_EQUAL_INT(2, count_tokens_specific(str));
 }
 
 void	check_token_count_parallel_quotes()
@@ -31,27 +57,86 @@ void	check_token_count_detached_quote()
 
 void	check_token_count_pipe()
 {
-	TEST_ASSERT_EQUAL_INT(4, 4);
-	TEST_ASSERT_EQUAL_INT(4, 4);
+	char	*str;
+	TEST_ASSERT_EQUAL_INT(2, count_tokens_specific(TOKENS_GENERAL_2));
+	
+	str = "ls -l | wc -c";
+	TEST_ASSERT_EQUAL_INT(1, count_tokens_specific(str));
+
+	str = "ls -la..|cat -e|wc -l";
+	TEST_ASSERT_EQUAL_INT(2, count_tokens_specific(str));
+
+	str = "ls -l|wc -c";
+	TEST_ASSERT_EQUAL_INT(1, count_tokens_specific(str));
 }
 
 void	check_token_count_greaterthan()
 {
-	TEST_ASSERT_EQUAL_INT(3, 3);
-	TEST_ASSERT_EQUAL_INT(3, 3);
+	char	*str;
+
+	str = "txt<oi<ls";
+	TEST_ASSERT_EQUAL_INT(2, count_tokens_specific(str));
+
+	str = "txt < oi < ls";
+	TEST_ASSERT_EQUAL_INT(2, count_tokens_specific(str));
+
+	str = "< oi < ls";
+	TEST_ASSERT_EQUAL_INT(2, count_tokens_specific(str));
+
+	str = "< <";
+	TEST_ASSERT_EQUAL_INT(2, count_tokens_specific(str));
+
+	str = "< < < < < < < < < < <";
+	TEST_ASSERT_EQUAL_INT(11, count_tokens_specific(str));
+
+	str = "txt<oi<ls<ks<ksk<kddk<sjsd";
+	TEST_ASSERT_EQUAL_INT(6, count_tokens_specific(str));
 }
 
 void	check_token_count_lessthan()
 {
-	TEST_ASSERT_EQUAL_INT(4, 4);
-	TEST_ASSERT_EQUAL_INT(3, 3);
-	TEST_ASSERT_EQUAL_INT(3, 3);
-	TEST_ASSERT_EQUAL_INT(4, 4);
+	char	*str;
+
+	str = "txt>oi>ls";
+	TEST_ASSERT_EQUAL_INT(2, count_tokens_specific(str));
+
+	str = "txt > oi > ls";
+	TEST_ASSERT_EQUAL_INT(2, count_tokens_specific(str));
+
+	str = "> oi > ls";
+	TEST_ASSERT_EQUAL_INT(2, count_tokens_specific(str));
+
+	str = "> >";
+	TEST_ASSERT_EQUAL_INT(2, count_tokens_specific(str));
+
+	str = "> > > > > > > > > > >";
+	TEST_ASSERT_EQUAL_INT(11, count_tokens_specific(str));
+
+	str = "txt>oi>ls>ks>ksk>kddk>sjsd";
+	TEST_ASSERT_EQUAL_INT(6, count_tokens_specific(str));
 }
 
 void	check_token_count_double_greaterthan()
 {
-	TEST_ASSERT_EQUAL_INT(4, 4);
+	char	*str;
+
+	str = "txt>>oi>>ls";
+	TEST_ASSERT_EQUAL_INT(2, count_tokens_specific(str));
+
+	str = "txt >> oi >> ls";
+	TEST_ASSERT_EQUAL_INT(2, count_tokens_specific(str));
+
+	str = ">> oi >> ls";
+	TEST_ASSERT_EQUAL_INT(2, count_tokens_specific(str));
+
+	str = ">> >>";
+	TEST_ASSERT_EQUAL_INT(2, count_tokens_specific(str));
+
+	str = ">> >> >> >> >> >> >> >> >> >> >>";
+	TEST_ASSERT_EQUAL_INT(11, count_tokens_specific(str));
+
+	str = "txt>>oi>>ls>>ks>>ksk>>kddk>>sjsd";
+	TEST_ASSERT_EQUAL_INT(6, count_tokens_specific(str));
 }
 
 void	check_token_count_double_lessthan()
@@ -85,14 +170,15 @@ void	check_token_count_redirection(void)
 	check_token_count_double_lessthan();
 }
 
-void	check_token_qtt(void)
-{
-	RUN_TEST(check_token_count);
-}
 
 void	check_token_count(void)
 {
 	check_token_count_general();
 	check_token_count_quotes();
 	check_token_count_redirection();
+}
+
+void	check_token_qtt(void)
+{
+	RUN_TEST(check_token_count);
 }
