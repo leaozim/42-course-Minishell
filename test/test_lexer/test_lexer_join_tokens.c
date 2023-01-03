eaaee52d7635777463c0cd407c9781bc8ac2d7c2
Larissa <larissa_silva@outlook.com>
2023-01-03 19:02:15 -0300

    WIP: add tests join tokens

--- test/test_lexer/test_lexer_join_tokens.c
+++ test/test_lexer/test_lexer_join_tokens.c
@@ -9,37 +9,19 @@ void test_join_token(char *prompt, char **expected)
 	ms.prompt_line = ft_strdup(prompt);
 	create_tokens();
 	parser();
-	// expander();
 	node = ms.tks;
 	i = 0;
 	while (node)
+
 	{
 		tklist = (t_tokens *)node->content;
 		TEST_ASSERT_EQUAL_STRING(expected[i], tklist->token);
 		node = node->next;
 		i++;
+		printf("%d\n", i);
 	}
 	free(ms.prompt_line);
 }
-
-
-// tokens normais
-// //oi
-// //oi ola
-// //oi ola tudo
-// //oi < tudo
-// //oi<tudo
-
-
-// conjunto único de aspas
-// //"oi"
-// //"oi""ola"
-// //"ola" "tudo"
-// //"oi"ola
-// //oi"ola"
-// //"oi" ola
-// //oi "ola"
-
 
 // múltiplos conjuntos de aspas
 // //"ola""tudo""bem"
@@ -90,9 +72,148 @@ void test_join_token(char *prompt, char **expected)
 
 void	test_valid_join_token(void)
 {
-	test_join_token("\"s\"\"-la\"", (char *[]){"\"s\"\"-la\"", NULL});
+	//                  tokens normais                    //
+	test_join_token("oi ola", (char *[]){"oi", "ola", NULL});
 	free_ms();
 
-	test_join_token("oi \"ola\"\"tudo\"\"bem\" com", (char *[]){"oi", "\"ola\"\"tudo\"\"bem\"", "com", NULL});
+	test_join_token("oi ola tudo", (char *[]){"oi", "ola", "tudo", NULL});
 	free_ms();
+
+	test_join_token("oi < tudo", (char *[]){"oi", "<", "tudo", NULL});
+	free_ms();
+
+	test_join_token("oi<tudo", (char *[]){"oi", "<", "tudo", NULL});
+	free_ms();
+	
+	//                    conjunto único de aspas                     //
+
+	test_join_token("oi", (char *[]){"oi", NULL});
+	free_ms();
+
+	test_join_token("\"oi\"\"ola\"", (char *[]){"\"oi\"\"ola\"", NULL});
+	free_ms();
+
+	// test_join_token("\"oi\" \"tudo\"", (char *[]){"\"oi\"", "\"tudo\"", NULL});
+	// free_ms();
+
+	test_join_token("\"oi\"ola", (char *[]){"\"oi\"ola", NULL});
+	free_ms();
+
+	test_join_token("oi\"ola\"", (char *[]){"oi\"ola\"", NULL});
+	free_ms();
+	
+	test_join_token("\"oi\" ola", (char *[]){"\"oi\"", "ola", NULL});
+	free_ms();
+
+	test_join_token("oi \"ola\"", (char *[]){"oi","\"ola\"", NULL});
+	free_ms();
+
+
+	
+	//                    conjunto multiplos de aspas                     //
+	
+	test_join_token("\"ola\"\"tudo\"\"bem\"", (char *[]){"\"ola\"\"tudo\"\"bem\"", NULL});
+	free_ms(); 
+
+	// test_join_token("\"ola\"\"tudo\"\"bem\"\"com\"", (char *[]){"\"ola\"\"tudo\"\"bem\"\"com\"", NULL});
+	// free_ms();
+
+	// test_join_token("\"ola\"\"tudo\"\"bem\"\"com\"\"voce\"", (char *[]){"\"ola\"\"tudo\"\"bem\"\"com\"\"voce\"", NULL});
+	// free_ms();
+
+	// test_join_token("\"ola\"\"tudo\"\"bem\"\"com\"\"voce\"\"?\"", (char *[]){"\"ola\"\"tudo\"\"bem\"\"com\"\"voce\"\"?\"", NULL});
+	// free_ms();
+
+
+	//                    conjunto separados                    //
+
+	//esses casos de erro o test nao pega:
+	// test_join_token("\"ola\" \"tudo\" \"bem\"", (char *[]){"\"ola\"", "\"tudo\"", "\"bem\"", NULL});
+	// free_ms();
+
+	// test_join_token("\"ola\" \"tudo\" \"bem\" \"com\"", (char *[]){"\"ola\"", "\"tudo\"", "\"bem\"", "\"com\"", NULL});
+	// free_ms();
+
+	// 	test_join_token("\"ola\" \"tudo\" \"bem\" \"com\" \"?\"", (char *[]){"\"ola\"", "\"tudo\"", "\"bem\"", "\"com\"", "\"?\"", NULL});
+	// free_ms();
+
+
+	//                    conjunto de aspas (2) entre tokens normais                    //
+
+	// test_join_token("oi \"ola\"tudo bem", (char *[]){"oi", "\"ola\"tudo", "bem", NULL});
+	// free_ms();
+
+	// test_join_token("oi ola\"tudo\" bem", (char *[]){"oi", "ola\"tudo\"", "bem", NULL});
+	// free_ms();
+
+	// test_join_token("oi \"ola\"\"tudo\" bem", (char *[]){"oi", "\"ola\"\"tudo\"", "bem", NULL});
+	// free_ms();
+
+	// test_join_token("\"oi\" \"ola\"\"tudo\" "\bem\"", (char *[]){"\"oi\"", "\"ola\"\"tudo\"", ""\bem\"", NULL});
+	// free_ms();
+
+	// test_join_token("\"oi\" ola\"tudo\" "\bem\"", (char *[]){"\"oi\"", "ola\"tudo\"", ""\bem\"", NULL});
+	// free_ms();
+
+	// test_join_token("\"oi\" \"ola\"\"tudo\" "\bem\"", (char *[]){"\"oi\"", "\"ola\"\"tudo\"", ""\bem\"", NULL});
+	// free_ms();
+
+	// test_join_token("oi \"ola\"\"tudo\" \"bem\"", (char *[]){"oi", "\"ola\"\"tudo\"", "\"bem\"", NULL});
+	// free_ms();
+
+	// test_join_token("\"oi\" \"ola\"\"tudo\" bem", (char *[]){"\"oi\"", "\"ola\"\"tudo\"", "bem", NULL});
+	// free_ms();
+
+	// test_join_token("oi ola\"tudo\" \"bem\"", (char *[]){"oi", "ola\"tudo\"", "\"bem\"", NULL});
+	// free_ms();
+
+	// test_join_token("oi \"ola\"tudo \"bem\"", (char *[]){"\"oi\"", "\"ola\"tudo", "\"bem\"", NULL});
+	// free_ms();
+
+	// test_join_token("\"oi\" \"ola\"tudo bem", (char *[]){"\"oi\"", "\"ola\"tudo", "bem", NULL});
+	// free_ms();
+
+	// test_join_token("\"oi\" ola\"tudo\" bem", (char *[]){"\"oi\"", "ola\"tudo\"", "bem", NULL});
+	// free_ms();
+
+	//                    conjunto de aspas (3) entre tokens normais                    //
+
+	// test_join_token("oi \"ola\"\"tudo\"\"bem\" com", (char *[]){"oi", "\"ola\"\"tudo\"\"bem\"", "com", NULL});
+	// free_ms();
+
+	// test_join_token("oi ola\"tudo\"\"bem\" com", (char *[]){"oi", "ola\"tudo\"\"bem\"", "com", NULL});
+	// free_ms();
+
+	// test_join_token("oi ola\"tudo\"bem com", (char *[]){"oi", "ola\"tudo\"bem", "com", NULL});
+	// free_ms();
+
+	// test_join_token("oi \"ola\"tudo\"bem\" com", (char *[]){"oi", "\"ola\"tudo\"bem\"", "com", NULL});
+	// free_ms();
+
+	// test_join_token("\"oi\" \"ola\"\"tudo\"\"bem\" \"com\"", (char *[]){"\"oi\"", "\"ola\"\"tudo\"\"bem\"", "\"com\"", NULL});
+	// free_ms();
+
+	// test_join_token("\"oi\" ola\"tudo\"\"bem\" \"com\"", (char *[]){"\"oi\"", "\"ola\"tudo\"\"bem\"", "\"com\"", NULL});
+	// free_ms();
+
+	// test_join_token("\"oi\" \"ola\"tudo\"bem\" \"com\"", (char *[]){"\"oi\"", "\"ola\"tudo\"bem\"", "\"com\"", NULL});
+	// free_ms();
+
+	// 	test_join_token("\"oi\" \"ola\"tudo\"bem\" com", (char *[]){"\"oi\"", "\"ola\"tudo\"bem\"", "com", NULL});
+	// 	free_ms();
+
+	// 	test_join_token("\"oi\" ola\"tudo\"\"bem\" com", (char *[]){"\"oi\"", "\"ola\"tudo\"\"bem\"", "com", NULL});
+	// 	free_ms();
+
+	// 	test_join_token("\"oi\" \"ola\"tudo\"bem\" com", (char *[]){"\"oi\"", "\"ola\"tudo\"bem\"", "com", NULL});
+	// 	free_ms();
+
+	// test_join_token("oi \"ola\"\"tudo\"\"bem\" \"com\"", (char *[]){"oi", "\"ola\"\"tudo\"\"bem\"", "\"com\"", NULL});
+	// free_ms();
+
+	// test_join_token("oi ola\"tudo\"\"bem\" \"com\"", (char *[]){"oi", "ola\"tudo\"\"bem\"", "\"com\"", NULL});
+	// free_ms();
+
+	// test_join_token("oi ola\"tudo\"bem \"com\"", (char *[]){"oi", "ola\"tudo\"bem", "\"com\"", NULL});
+	// free_ms();
 }
