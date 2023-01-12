a1ef4c9250329fc66e5dc5a98eea8fe052367954
Larissa <larissa_silva@outlook.com>
2023-01-12 17:44:22 -0300

    resolved conflicts 2

--- src/executer/child_process.c
+++ src/executer/child_process.c
@@ -1,22 +1,5 @@
 #include "../../include/minishell.h"
 
-void	child_dup_redirection(int i)
-{
-	if (i == 0)
-	{
-		dup2(g_ms.array_fd[i][1], STDOUT_FILENO);
-	}
-	else if (i != g_ms.num_pipes)
-	{
-		dup2(g_ms.array_fd[i - 1][0], STDIN_FILENO);
-		dup2(g_ms.array_fd[i][1], STDOUT_FILENO);
-	}
-	else
-	{
-		dup2(g_ms.array_fd[i - 1][0], STDIN_FILENO);
-	}
-}
-
 void	child_process_execution(t_list *node)
 {
 	char	*path;
@@ -33,16 +16,17 @@ void	child_process_execution(t_list *node)
 	}
 }
 
-void	child_process_check(t_list *node, int i)
+void	child_process_check(t_list *node)
 {
 	if (check_path((t_commands *)node->content) == FALSE)
 	{
+		ft_putstr_fd("Minishell: ", STDERR_FILENO);
+		ft_putstr_fd(((t_commands *)node->content)->cmd_list[0], STDERR_FILENO);
+		ft_putendl_fd(": command not found", STDERR_FILENO);
 		g_ms.exit_status = COMMAND_NOT_FOUND;
 		free_cmd_data();
 		exit(g_ms.exit_status);
 	}
-	if (g_ms.num_pipes > 0)
-		child_dup_redirection(i);
 	close_pipes();
 	child_process_execution(node);
 }
