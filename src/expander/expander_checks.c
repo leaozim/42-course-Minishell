67e1775adc9c4bb2eb01914c4e965d62992860e7
Emmanuel Tomiyoshi <tomiyoshiemmanuel@gmail.com>
2023-01-14 15:29:12 -0300

    fixed dollar_sign status 1/2

--- src/expander/expander_checks.c
+++ src/expander/expander_checks.c
@@ -10,11 +10,28 @@ void	expand_check_next_character(char *token, int *i, char **final_str)
 	if (is_brace_expansion(token, i, final_str) == TRUE)
 		return ;
 	isdigit = ft_isalpha_underscore(token[*i + 1]);
-	if (isdigit == TRUE) //colocar opcao "|| token[i + 1] == QUERY"
+	if (isdigit == TRUE || token[*i + 1] == QUERY) //colocar opcao "|| token[i + 1] == QUERY"
 	{
 		start = *i;
 		while (ft_isalpha_underscore(token[*i + 1]))
 			*i += 1;
+		if (ft_strchr(token, QUERY) != NULL) //comeca
+		{
+			start = ft_strchr_pos(token, QUERY);
+			if (ft_strlen(token) == 2)
+			{
+				ft_strupdate(final_str, ft_itoa(g_ms.exit_status));
+				return ;
+			}
+			while (ft_isalpha_underscore(token[*i + 1]))
+				*i += 1;
+			aux = ft_substr(token, start + 1, (*i - start));
+			expanded_var = ft_itoa(g_ms.exit_status);
+			ft_strupdate(final_str, ft_strjoin(expanded_var, aux));
+			free(aux);
+			free(expanded_var);
+			return ;
+		} //termina
 		aux = ft_substr(token, (start + 1), (*i - start));
 		expanded_var = getenv(aux);
 		free(aux);
@@ -31,7 +48,7 @@ int	check_last_expansion_occurrence(char *token)
 	int	i;
 
 	i = ft_strrchr_pos(token, DOLLAR_SIGN);
-	while (ft_isalpha_underscore(token[i + 1]) || token[i + 1] == '{')
+	while (ft_isalpha_underscore(token[i + 1]) || token[i + 1] == '{' || token[i + 1] == QUERY)
 		i++;
 	if (token[i + 1] == '}')
 		i++;
